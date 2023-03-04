06b3bae4f73f502c173b0b07aa29f4acc545e36f
Mo Wasim <56387679+mowasim@users.noreply.github.com>
2023-03-04 01:02:41 +0000

    Add files via upload

---
+++ ModuleExample/terraform.tfvars
@@ -0,0 +1,4 @@
+base_name_module_rg      = "newrgfromdesk"
+base_name_module_storage = "modulezam"
+location_module_rg       = "east us"
+location_module_storage  = "east us"
---
+++ ModuleExample/variable.tf
@@ -0,0 +1,19 @@
+variable "base_name_module_rg" {
+  type        = string
+  description = "Base name for the RG"
+}
+
+variable "base_name_module_storage" {
+  type        = string
+  description = "Base name for the storage"
+}
+
+variable "location_module_rg" {
+  type        = string
+  description = "Location for RG"
+}
+
+variable "location_module_storage" {
+  type        = string
+  description = "Location for storage"
+}
